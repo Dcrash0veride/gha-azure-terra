52862ddc57d6cedf8f01dc4ba8715c3d20aa310e
Dcrash0veride <47066765+Dcrash0veride@users.noreply.github.com>
2024-03-03 14:04:50 -0600

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,17 @@ provider "azurerm" {
   features {}
 }
 
+provider "random" {
+
+}
+
 terraform {
+required_providers {
+  random = {
+    source = "hashicorp/random"
+    version = "3.6.0"
+  }
+}
   backend "azurerm" {
     resource_group_name = var.state_resource_group_name
     storage_account_name = var.state_storage_account_name
