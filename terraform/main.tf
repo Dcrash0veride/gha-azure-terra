dbb26d09a6a01885aedd5e67e6034dd67473cfac
Dcrash0veride <47066765+Dcrash0veride@users.noreply.github.com>
2024-03-04 17:18:14 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
-resource "azurerm_resource_group" var.resource_group {
-  name = 
+resource "random_id" "identity" {
+  byte_length = 8
 }
+
+resource "azurerm_resource_group" var.resource_group {
+  name = "sp-${random.id.identity.hex}"
+  location = "Central US"
+}
+
+resource "azurerm_user_assigned_identity" var.resource_group {
+  location = "Central US"
+  name
+} 
